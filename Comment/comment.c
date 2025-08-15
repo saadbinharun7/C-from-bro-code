bd565499baf47238d99bbfafa5afc209896f9282
Pha raoh <pharaoh1822005@gmail.com>
2025-08-15 14:41:24 +0600

    Ep 2 complete

---
+++ Comment/comment.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    //this is a comment
+    /*this
+      is
+      a
+      multiline
+      comment
+    */
+
+    /* Escape sequence = character combination consisting of a backslash \
+                         followed by aleter or combination of digits.
+                         they specify actions within a line or string of text
+                         \n = newline
+                         \t = tab
+    */
+    printf("I\nlike\npizza!\n");
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+
+    printf("\"I like pizza\" - Abraham Lincon probably "); // for " use \"
+                                                           // for ' use \'
+                                                           // for \ use \\
+
+
+
+    return 0;
+}
