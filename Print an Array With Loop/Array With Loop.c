20184be49375b6581eaab91bf23856dd0feb7521
Pha raoh <pharaoh1822005@gmail.com>
2025-08-16 22:06:47 +0600

    Ep 29 complete

---
+++ Print an Array With Loop/Array With Loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    double prices[] = {5.0,10.0,15.0,25.0};
+
+    for(int i = 0; i < sizeof(prices) / sizeof(prices[0]); i++){
+        printf("$%0.2lf\n", prices[i]);
+    }
+
+
+    return 0;
+}
