5fc9c07c82208c5e9c2be073ca05e041be86d4b7
Pha raoh <pharaoh1822005@gmail.com>
2025-08-16 19:51:17 +0600

    Ep 28 complete

---
+++ Arrays/Array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    //array - a data structure that can store many values of the same data type.
+
+    double prices[10] = {5.0,10.0,15.0,25.0,20.0};
+
+    printf("$%0.2lf", prices[4]);
+
+
+    return 0;
+}
